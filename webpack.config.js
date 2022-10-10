537c1a9122457b99a98cd365113925a941b39fbb
Niilo Keinänen <niilo.keinanen@lightningchart.com>
2022-10-10 15:29:51 +0300

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,53 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-module.exports = {
-    mode: 'development',
-    entry: './src/app.ts',
-    module:{
-        rules:[
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    optimization:{
-        splitChunks: {
-            chunks: 'all'
-        }
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'lcjs-typescript-example'
-        })
-    ]
-}
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const config = {
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: "ts-loader",
+        exclude: ["/node_modules/"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
+  },
+  externals: {
+    perf_hooks: "{}",
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
