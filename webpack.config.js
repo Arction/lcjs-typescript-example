f3029fc1cbeace229ca990cc89e2845197427474
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2022-11-09 08:47:31 +0200

    Change webpack hash function to sha256

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const isProduction = process.env.NODE_ENV == "production";
 const config = {
   entry: "./src/index.ts",
   output: {
+    hashFunction: 'sha256',
     path: path.resolve(__dirname, "dist"),
   },
   devServer: {
